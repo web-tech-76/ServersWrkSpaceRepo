69b5ce630330514fa71e47fc6d4f62f24b44463d
web-tech-76 <webtec76@gmail.com>
2020-07-02 22:30:15 +0530

    initial commit.

---
+++ eureka-server/src/test/java/org/eureka/services/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eureka.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
