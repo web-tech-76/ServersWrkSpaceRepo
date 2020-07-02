24f70b55ffaaea1c4860793127f74807762dff77
web-tech-76 <webtec76@gmail.com>
2020-07-02 22:39:11 +0530

    initial commit.

---
+++ spring-config-server/src/test/java/com/cloud/config/app/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.config.app;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
